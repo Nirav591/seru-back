8b05c2c9c3e4614d31e7a0b11515600ed3039a0b
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-30 12:20:53 +0000

    add

--- config/db.js
+++ config/db.js
@@ -2,10 +2,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: 'ls-e2029d66e023eb4cf5366724fe912a2e37ded960.czmq42a8qw3q.eu-west-2.rds.amazonaws.com',
+    user: 'dbmasteruser',  // Corrected username
+    password: 'London1995',  // Keep this secure; consider using environment variables
+    database: 'seru',  // Your database name
+    port: 3306,  // Ensure correct port
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
