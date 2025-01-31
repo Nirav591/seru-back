8c16c1b0295ad5b1be7825090dcf36172b544808
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-31 15:10:21 +0000

    added

--- validators/examQuestionValidators.js
+++ validators/examQuestionValidators.js
@@ -1,16 +1,17 @@
+// examQuestionValidators.js
 const Joi = require('joi');
 
 const optionSchema = Joi.object({
-    option: Joi.string().required(),
-    isAnswer: Joi.boolean().required(),
+  option_text: Joi.string().required(),
+  isAnswer: Joi.boolean().required()
 });
 
 const examQuestionSchema = Joi.object({
-    exam_test_id: Joi.number().required(),
-    question: Joi.string().required(),
-    type: Joi.string().valid('fill_in_blank', 'multiple_choice', 'true_false').required(),
-    noOfAnswer: Joi.number().required(),
-    options: Joi.array().items(optionSchema).required(),
+  exam_test_id: Joi.number().required(),
+  question: Joi.string().required(),
+  type: Joi.string().required(),
+  noOfAnswer: Joi.number().required(),
+  options: Joi.array().items(optionSchema).required()
 });
 
 module.exports = { examQuestionSchema };
