534d7e1b0486e64829785b7ec080756ccd665307
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-31 15:21:48 +0000

    added

--- models/examOptionModel.js
+++ models/examOptionModel.js
@@ -3,12 +3,17 @@ const db = require('../config/db');
 class ExamOption {
     static async create(examOption) {
         const { exam_question_id, option, isAnswer } = examOption;
+
+        // Check if required fields are present
+        if (!exam_question_id || !option || isAnswer === undefined) {
+            throw new Error('Missing required fields: exam_question_id, option, or isAnswer');
+        }
+
         await db.execute(
             'INSERT INTO exam_options (exam_question_id, `option`, isAnswer) VALUES (?, ?, ?)', // Escaped here
             [exam_question_id, option, isAnswer]
         );
     }
 }
-
 
 module.exports = ExamOption;
