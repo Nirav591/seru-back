68d18ea8f1952a58cf4dd0365b798790e852063f
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-31 16:08:23 +0000

    added

--- models/examOptionModel.js
+++ models/examOptionModel.js
@@ -3,14 +3,8 @@ const db = require('../config/db');
 class ExamOption {
     static async create(examOption) {
         const { exam_question_id, option, isAnswer } = examOption;
-
-        // Check if required fields are present
-        if (!exam_question_id || !option || isAnswer === undefined) {
-            throw new Error('Missing required fields: exam_question_id, option, or isAnswer');
-        }
-
         await db.execute(
-            'INSERT INTO exam_options (exam_question_id, `option`, isAnswer) VALUES (?, ?, ?)', // Escaped here
+            'INSERT INTO exam_options (exam_question_id, option, isAnswer) VALUES (?, ?, ?)',
             [exam_question_id, option, isAnswer]
         );
     }
--- validators/examQuestionValidators.js
+++ validators/examQuestionValidators.js
@@ -1,16 +1,16 @@
 const Joi = require('joi');
 
 const optionSchema = Joi.object({
-  option: Joi.string().required(),
-  isAnswer: Joi.boolean().required(),
+    option: Joi.string().required(),
+    isAnswer: Joi.boolean().required(),
 });
 
 const examQuestionSchema = Joi.object({
-  exam_test_id: Joi.number().required(),
-  question: Joi.string().required(),
-  type: Joi.string().valid('multiple_choice', 'true_false').required(),
-  noOfAnswer: Joi.number().required(),
-  options: Joi.array().items(optionSchema).required(),
+    exam_test_id: Joi.number().required(),
+    question: Joi.string().required(),
+    type: Joi.string().valid('fill_in_blank', 'multiple_choice', 'true_false').required(),
+    noOfAnswer: Joi.number().required(),
+    options: Joi.array().items(optionSchema).required(),
 });
 
 module.exports = { examQuestionSchema };
