30fd0ebbd35732aed145c8bc14c66e508bb7755d
Nirav591 <nirav.v.lathiya@gmail.com>
2025-01-31 16:17:32 +0000

    added

--- models/examOptionModel.js
+++ models/examOptionModel.js
@@ -4,7 +4,7 @@ class ExamOption {
     static async create(examOption) {
         const { exam_question_id, option, isAnswer } = examOption;
         await db.execute(
-            'INSERT INTO exam_options (exam_question_id, option, isAnswer) VALUES (?, ?, ?)',
+            'INSERT INTO exam_options (exam_question_id, `option`, isAnswer) VALUES (?, ?, ?)', // Escaped here
             [exam_question_id, option, isAnswer]
         );
     }
