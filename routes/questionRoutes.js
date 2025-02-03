f9c376c337e1916a4c6bb2772c5efccfd375ff7d
Nirav591 <nirav.v.lathiya@gmail.com>
2025-02-03 14:17:55 +0000

    route

--- routes/questionRoutes.js
+++ routes/questionRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const { createQuestion , getAllQuestions, getQuestionsByChapterId , deleteQuestion, editQuestion} = require('../controllers/questionController');
+
 const router = express.Router();
-const { createQuestion, getAllQuestions, getQuestionsByChapterId, deleteQuestion, editQuestion } = require('../controllers/questionController');
 
 router.post('/questions', createQuestion);
+
 router.get('/questions', getAllQuestions);
-router.get('/questions/:chapter_id', getQuestionsByChapterId);
+
+router.get('/questions/chapter/:chapter_id', getQuestionsByChapterId);
+
 router.delete('/questions/:id', deleteQuestion);
 router.put('/questions/:id', editQuestion);
+
+
+
 
 module.exports = router;
