2dfff87e90fa4740ca6017e4c0c9f4d10779e9a5
Nirav591 <nirav.v.lathiya@gmail.com>
2025-02-04 12:19:23 +0000

    add

--- models/examQuestionModel.js
+++ models/examQuestionModel.js
@@ -1,25 +1,3 @@
-const getExamQuestionsByExamTestId = async (req, res) => {
-    try {
-        const { exam_test_id } = req.params;
-        console.log('Fetching questions for exam test ID:', exam_test_id);
-
-        const questions = await ExamQuestion.findByExamTestId(exam_test_id);
-        if (questions.length === 0) {
-            return res.status(404).json({ message: 'No questions found for this exam test' });
-        }
-
-        // Fetch options for each question
-        for (let question of questions) {
-            question.options = await ExamQuestion.getOptionsByQuestionId(question.id);
-        }
-
-        res.status(200).json({ questions });
-    } catch (error) {
-        console.error('Error in getExamQuestionsByExamTestId:', error);
-        res.status(500).json({ message: 'Server error' });
-    }
-};
-
 const db = require('../config/db');
 
 class ExamQuestion {
@@ -44,14 +22,8 @@ class ExamQuestion {
         );
         return rows[0]; // Return the first matching question
     }
+    
 
-    static async getOptionsByQuestionId(question_id) {
-        const [rows] = await db.execute(
-            'SELECT id, question_id, `option`, isAnswer FROM question_options WHERE question_id = ?',
-            [question_id]
-        );
-        return rows; // Return all options for the question
-    }
 }
 
 module.exports = ExamQuestion;
