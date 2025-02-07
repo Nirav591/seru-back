f92be0a9d5312abb0bc9ee9615035752af499e0a
Nirav591 <nirav.v.lathiya@gmail.com>
2025-02-07 14:11:49 +0000

    added

--- routes/examQuestionRoutes.js
+++ routes/examQuestionRoutes.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const { createExamQuestion , getExamQuestionsByExamTestId, getQuestionsByExamTestId, deleteExamQuestion} = require('../controllers/examQuestionController');
+const { createExamQuestion, getExamQuestionsByExamTestId, deleteExamQuestion } = require('../controllers/examQuestionController');
 
 const router = express.Router();
 
 // POST API to create a question for an exam test
 router.post('/exam-tests/:exam_test_id/questions', createExamQuestion);
-router.get('/exam-tests/:exam_test_id/questions', getQuestionsByExamTestId);
 
+// GET API to fetch questions with options and total count
 router.get('/exam-tests/:exam_test_id/questions', getExamQuestionsByExamTestId);
-router.delete('/exam-questions/:exam_question_id', deleteExamQuestion);
 
+// DELETE API to remove a question by ID
+router.delete('/exam-questions/:exam_question_id', deleteExamQuestion);
 
 module.exports = router;
