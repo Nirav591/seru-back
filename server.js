a21d7ab907b5f5adc055be506b86ebafc00e3a44
Nirav591 <nirav.v.lathiya@gmail.com>
2025-03-27 12:12:41 +0000

    add

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ const app = express();  // Move this line up to define 'app' before using it
 const allowedOrigins = [
     "http://localhost:3039",
     "http://13.40.120.157:6340",
-    "https://sheru.solidblackabroad.com"
+    "https://sheru.solidblackabroad.com",
+    "admin.solidblackabroad.com"
 ];
 
 // CORS middleware should come after defining 'app'
