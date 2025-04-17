667678e6b2915b173103bb7b2a4913ba92b2b9f3
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-17 23:38:11 +0100

    added

--- routes/exam.routes.js
+++ routes/exam.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const examController = require("../controllers/exam.controller");
 
 const validate = require("../middleware/validate"); // ✅ validate middleware
-const { questionArraySchema } = require("../validators/examQuestionValidator"); // ✅ this is the fix
+const { questionArraySchema } = require("../validators/examQuestionValidator");// ✅ this is the fix
 
 router.post("/", examController.createExam);
 router.post("/:examId/questions", validate(questionArraySchema), examController.addQuestionsToExam);
