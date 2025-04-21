bbc28f6e8a78d0f3a63197e3b482415feed27aa5
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-21 11:50:53 +0100

    added

--- server.js
+++ server.js
@@ -5,5 +5,5 @@ const chapterRoutes = require('./routes/chapter.routes');
 app.use(express.json()); // parse JSON body
 app.use('/api', chapterRoutes); // base route for chapter APIs
 
-const PORT = process.env.PORT || 5000;
+const PORT = 6340;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
