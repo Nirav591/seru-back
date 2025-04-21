03b96d542bf17cf9d523c1509ba005ecc14324d9
Nirav591 <nirav.v.lathiya@gmail.com>
2025-04-21 14:40:19 +0100

    added

--- server.js
+++ server.js
@@ -3,9 +3,16 @@ const app = express();
 const chapterRoutes = require('./routes/chapter.routes');
 const questionRoutes = require('./routes/question.routes');
 const examRoutes = require('./routes/exam.routes');
+const cors = require('cors');
+
+
+app.use(cors());
 
 
 app.use(express.json()); // parse JSON body
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use('/api', chapterRoutes);
 app.use('/api', questionRoutes);
 app.use('/api', examRoutes);
